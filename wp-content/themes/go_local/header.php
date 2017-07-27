47ae4f674be8e0b71a3064b5f25350417853f851
matan alkalay <matan.alkalay@gmail.com>
2017-07-27 10:31:42 +0300

    test

--- wp-content/themes/go_local/header.php
+++ wp-content/themes/go_local/header.php
@@ -1,56 +1,29 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package go_local
- */
-
-?>
-<!doctype html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta name="description" content="<?php bloginfo('description'); ?>">
+	<title><?php echo get_bloginfo('name'); ?> - <?php wp_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
-
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'go_local' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'go_local' ); ?></button>
-			<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+<body <?php body_class('mybodyclass'); ?>>
+<div class="container">
+<div class="row">
+<header>
+	<?php
+	$header_menu_name = is_user_logged_in() ? 'primary' : 'guest';
+    if(has_nav_menu($header_menu_name)) {
+		$header_menu = array(
+      'theme_location' => $header_menu_name,
+      'container' => '',
+      'container_class' => '',
+      'container_id' => '',
+      'menu_class' => '',
+      'echo' => true,
+		);
+		wp_nav_menu($header_menu);
+    }
+	?>
+</header>
+</div>
+</div>
