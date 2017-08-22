1418910d11fccf3ddb5818e440464369b35102f1
matan alkalay <matan.alkalay@gmail.com>
2017-08-22 12:44:56 +0300

    section4 -3 have been added

---
+++ wp-content/themes/go_local/section3.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style type="text/css">
+		#section3 {
+			background-image: url("/images/startup-593327_1920-min.jpg");
+			background-color: orange;
+		
+		}
+		.section-heading {
+		display: inline-block;
+	    padding-bottom: 2%;
+	    text-shadow: 2px 2px 5px #9E9E9E;
+	}
+	.section-subheading {
+		font-size: x-large;
+	    margin-top: 5%;
+	    margin-bottom: 5%;
+	}
+	.sevice-col-wrapper {
+		margin-bottom: 10%;
+	}
+	.circle1 {
+		margin-bottom: 10%;
+		width: 100px;
+	    height: 100px;
+	    border-radius: 50%;
+	    display: inline-block;
+	}
+	.circle1 i {
+		font-size: -webkit-xxx-large;
+	    color: white;
+	    margin-top: 24%;
+	}
+	.circle2 {
+		margin-bottom: 10%;
+		width: 100px;
+	    height: 100px;
+	    border-radius: 50%;
+	    display: inline-block;
+	}
+	.circle2 i {
+		font-size: -webkit-xxx-large;
+	    color: white;
+	    margin-top: 24%;
+	}
+	.circle3 {
+		margin-bottom: 10%;
+		width: 100px;
+	    height: 100px;
+	    border-radius: 50%;
+	    display: inline-block;
+	}
+	.circle3 i {
+		font-size: -webkit-xxx-large;
+	    color: white;
+	    margin-top: 24%;
+	}
+	.service-heading {
+		color: white;
+    	margin-bottom: 10%;
+    }
+    .service-subheading {
+    	color: white;
+    }
+    </style>
+    </head>
+<body>
+<section id="section3" data-center="background-position: 50% 0px;" data-top-bottom="background-position: 50% -100px;" class="plrx_enable">
+<section id="section3"> 
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">אודות התוכנית שלנו</h2>
+                <h3 class="section-subheading">לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית קולורס מונפרד אדנדום סילקוף, מרגשי ומרגשח. עמחליף קונדימנטום קורוס בליקרה </h3>
+            </div>
+        </div>
+        <div class="row text-center servies wow fadeInRight" data-wow-duration="1s">
+            <div class="col-md-4 sevice1">
+            <div class="sevice-col-wrapper">
+               <div class="circle1">
+              	 	<i class="fa fa-phone" aria-hidden="true"></i>
+               </div>
+                <h4 class="service-heading">מיקור חוץ</h4>
+                <p class="service-subheading">לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית קולורס מונפרד  </p>
+            </div>
+            </div>
+            <div class="col-md-4 sevice2">
+			<div class="sevice-col-wrapper">
+                <div class="circle2">
+				<i class="fa fa-user-circle" aria-hidden="true"></i>               
+				</div>
+                <h4 class="service-heading">מערכות מורכבות</h4>
+                <p class="service-subheading">לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית קולורס מונפרד </p>
+            </div>
+            </div>
+            <div class="col-md-4 sevice3">
+			<div class="sevice-col-wrapper">
+               <div class="circle3">
+              	 	<i class="fa fa-commenting-o" aria-hidden="true"></i>
+               </div>
+                <h4 class="service-heading">אתרי תדמית ותוכן</h4>
+                <p class="service-subheading">לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית קולורס מונפרד</p> 
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+</section>
---
+++ wp-content/themes/go_local/section4.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<style type="text/css">
+  #section4 {
+      background-image: url("/images/comp.jpg");
+      
+    }
+    #section4 .circle1 {
+      margin-bottom: 0%!important;
+    width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      display: inline-block;
+      background: #fc4a1a;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #f7b733, #fc4a1a);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #f7b733, #fc4a1a); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+      border: 1px solid #f7b733;
+      float: right;
+    }
+    #section4 .circle2 {
+      margin-bottom: 0%!important;
+    width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      display: inline-block;
+      background: #fc4a1a;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #f7b733, #fc4a1a);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #f7b733, #fc4a1a); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+      border: 1px solid #f7b733;
+      float: right;
+    }
+    #section4 .circle3 {
+      margin-bottom: 0%!important;
+    width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      display: inline-block;
+      background: #fc4a1a;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #f7b733, #fc4a1a);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #f7b733, #fc4a1a); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+      border: 1px solid #f7b733;
+      float: right;
+    }
+    #section4 .service-heading {
+      display: inline-block;
+      float: right;
+      margin-bottom: 8%;
+      margin-top: 3%;
+      margin-right: 6%;
+    }
+    #section4 .sevice-col-wrapper {
+    margin-bottom: 0%!important;
+  }
+  #section4 .circle1 i {
+    margin-top: 35%;
+      margin-right: 20%;
+  }
+  #section4 .circle2 i {
+    margin-top: 35%;
+      margin-right: 13%;
+  }
+  #section4 .circle3 i {
+    margin-top: 35%;
+      margin-right: 13%;
+  }
+  </style>
+</style>
+<section id="section4" data-center="background-position: 50% 0px;" data-top-bottom="background-position: 50% -100px;" class="plrx_enable">
+<section id="section4"> 
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-right">
+                <h2 class="section-heading">אודות התוכנית - קידום להכשרה</h2>
+            </div>
+        </div>
+        <div class="row text-right servies wow fadeInRight" data-wow-duration="1s">
+            <div class="col-xs-12 sevice1">
+            <div class="col-xs-12 sevice-col-wrapper">
+               <div class="col-xs-6 circle1">
+              	 	<i class="fa fa-phone" aria-hidden="true"></i>
+               </div>
+                <h4 class="col-xs-6 service-heading">ורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית קוואזי במר מודוף. אודיפו בלאסטיק מונופץ קליר, בנפת נפקט למסון בלרק</h4>
+            </div>
+            </div>
+            <div class="col-xs-12 sevice2">
+			<div class="col-xs-12 sevice-col-wrapper">
+                <div class="col-xs-6 circle2">
+				<i class="fa fa-user-circle" aria-hidden="true"></i>               
+				</div>
+                <h4 class="col-xs-6 service-heading">ורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית קוואזי במר מודוף. אודיפו בלאסטיק מונופץ קליר, בנפת נפקט למסון בלרק</h4>   
+            </div>
+            </div>
+            <div class="col-xs-12 sevice3">
+			<div class="col-xs-12 sevice-col-wrapper">
+               <div class="col-xs-6 circle3">
+              	 	<i class="fa fa-commenting-o" aria-hidden="true"></i>
+               </div>
+                <h4 class="col-xs-6 service-heading">ורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית קוואזי במר מודוף. אודיפו בלאסטיק מונופץ קליר, בנפת נפקט למסון בלרק</h4> 
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+</section>
+
+
+
+
+
+
+
+
+</body>
+</html>
